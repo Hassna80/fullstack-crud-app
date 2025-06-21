0f64318455eda808b1e6e22046c89746c1f2e5f4
hasnaaoubouh <hassnaoubouh1@gmail.com>
2025-06-21 23:33:22 +0100

    initialize project

---
+++ src/main/java/com/example/taskborad/TaskBoradApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskborad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskBoradApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskBoradApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/taskborad/TaskBoradApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskborad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskBoradApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
