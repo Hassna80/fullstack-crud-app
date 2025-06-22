5a60d4910e01111473ff6b827e4e19b96eec5eff
hasnaaoubouh <hassnaoubouh1@gmail.com>
2025-06-22 18:34:36 +0100

    add crud endpoints

--- src/main/java/com/example/taskborad/api/TaskBoardApi.java
+++ src/main/java/com/example/taskborad/api/TaskBoardApi.java
@@ -8,13 +8,14 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api")
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class TaskBoardApi {
 
     @Autowired
     TaskService taskService;
 
-    @PostMapping("/task")
+    @PostMapping("/add")
     public Task saveTask(@RequestBody Task task) {
         return taskService.saveTask(task);
     }
@@ -26,13 +27,13 @@ public class TaskBoardApi {
     }
 
 
-    @PutMapping("tasks/{id}")
+    @PutMapping("/update/{id}")
     public Task updateTask(@RequestBody Task task, @PathVariable Long id) {
         return taskService.updateTask(task, id);
     }
 
 
-    @DeleteMapping("tasks/{id}")
+    @DeleteMapping("/delete/{id}")
     public void deleteTask(@PathVariable Long id) {
         taskService.deleteTask(id);
     }
